3047611ebac0479817d63fbc6d2816cad8407b8f
FoboIX <89666151+fmills89@users.noreply.github.com>
2021-12-06 19:48:58 -0600

    city daily forecast displayong  added minor css styling

--- assets/app.js
+++ assets/app.js
@@ -2,13 +2,15 @@ var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector("#city")
 var cityContainerEl = document.querySelector("#city-container");
 var citySearchTerm = document.querySelector("#city-search-term");
+var forecastContainerEl = document.querySelector("#forecast-container");
 
-var APIkey = "a72e3fb63a67ce75037508eeb42b61ef";
+
+let APIkey = "a72e3fb63a67ce75037508eeb42b61ef";
 
 var formSubmitHandler = (e) => {
     e.preventDefault();
     // get value from input element
-    var cityName = cityInputEl.value.trim();
+    let cityName = cityInputEl.value.trim();
 
     if (cityName) {
         getCityDetails(cityName);
@@ -25,13 +27,99 @@ var getCityDetails = (city) => {
     fetch(apiURL).then(function (response){
         if (response.ok) {
             response.json().then(function (data){
-            console.log(data);
+            //console.log(data);
+
+        // creating vars for getWeatherDetails function
+        var cityName = (data.name);
+        var currentLat = (data.coord.lat);
+        var currentLon = (data.coord.lon);
+            console.log(currentLat);
+
+            getWeatherDetails(cityName, currentLat, currentLon);
         });
         } else {
             alert("Error, this city could not be found.");
         }
     });
 };
+
+var getWeatherDetails = (city, currentLat, currentLon) => {
+    var oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + currentLat + "&lon=" + currentLon + "&units=imperial&exclude=minutely,hourly&appid=" + APIkey;
+
+    fetch(oneCallUrl).then( (response) => {
+        response.json().then( (data) => {
+            console.log(data);
+            
+        displayCityDetails(data, city);
+        displayForcasted(data);
+
+        });
+    });
+};
+
+var displayCityDetails = (data, city) => {
+
+    var currentTemp = Math.round(data.current.temp);
+    console.log(currentTemp);
+    var currentWind = Math.round(data.current.wind_speed);
+    console.log(currentWind);
+    var currentHumidity = (data.current.humidity);
+    var currentUV = (data.current.uvi);
+    var weatherIcon = (data.current.weather[0].icon)
+    console.log(weatherIcon);
+    
+
+    cityContainerEl.textContent = "";
+    var divCityHeader = document.createElement("div");
+    var headerCityDateEl = document.createElement("h2");
+    var currentDate = moment().format('L');
+    var currentIcon = document.createElement("img");
+    currentIcon.setAttribute('src', '');
+    currentIcon.setAttribute('src', "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png" );
+
+    headerCityDateEl.textContent = city + " (" + currentDate + ")";
+    headerCityDateEl.setAttribute("class", "card-header");
+
+    divCityHeader.appendChild(headerCityDateEl);
+    divCityHeader.appendChild(currentIcon);
+    cityContainerEl.appendChild(divCityHeader);
+
+
+    var divCurrent = document.createElement("div");
+    var tempEl = document.createElement("p");
+    var windEl = document.createElement("p");
+    var humidityEl = document.createElement("p");
+    var UvEl = document.createElement("p");
+
+    tempEl.textContent = "Temperature: " + currentTemp + "°F";
+    windEl.textContent = "Wind: " + currentWind + " MPH";
+    humidityEl.textContent = "Humidity: " + currentHumidity + "%";
+    UvEl.textContent = "UV Index: " + currentUV;
+
+    divCurrent.appendChild(tempEl);
+    divCurrent.appendChild(windEl);
+    divCurrent.appendChild(humidityEl);
+    divCurrent.appendChild(UvEl);
+
+    cityContainerEl.appendChild(divCurrent);
+
+};
+
+var displayForcasted = (data) => {
+
+    forecastContainerEl.textContent = "";
+
+    for (let i = 0; i < 5; i++) {
+        let cityForecast = Math.round(data.daily[i].temp.day);
+        let windForecast = data.daily[i].wind_speed;
+        let humidityForecast = data.daily[i].humidity;
+        
+       
+
+    }
+};
+
+
 
 
 
